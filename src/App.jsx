03c69ed2f33fe85995620367b5543ec7715b479c
Kamlesh Dilip Tankar <kamleshtankar96@gmail.com>
2024-11-06 22:02:39 +0530

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.scss'
+
+import Nav from './components/nav/Nav'
+import Hero from './components/hero/Hero'
+import Testimonal from './components/testimonal/Testimonals'
+import Advantage from './components/advantage/Advantage'
+import About from './components/about/About'
+import Banner from './components/banner/banner'
+import Contact from './components/contact/Contact'
+
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <Testimonal />
+      <Advantage />
+      <About />
+      <Banner />
+      <Contact />
+    </>
+  )
+}
+
+export default App
